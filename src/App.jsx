b004e5038878d7826d887e59e65b38a18083676b
MAjay2 <adjeimichael405@gmail.com>
2023-11-23 12:20:07 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Form from "../src/component/Form"
+
+function App() {
+ 
+
+  return (
+    <>
+     <Form/>
+    </>
+  )
+}
+
+export default App
